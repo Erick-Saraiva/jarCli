bfdb86f2b35040727352f2fec4906d3dbcd6e02b
Erick Saraiva <erick.ssilva@bandtec.com.br>
2022-05-23 11:13:26 -0300

    att

--- Cli/src/main/java/com/mycompany/loginhealthinspec/Cli.java
+++ Cli/src/main/java/com/mycompany/loginhealthinspec/Cli.java
@@ -2,50 +2,54 @@ package com.mycompany.loginhealthinspec;
 
 import com.github.britooo.looca.api.core.Looca;
 import java.io.File;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Cli {
 
     public static void main(String[] args) {
         Looca looca = new Looca();
-        Integer processList = looca.getGrupoDeProcessos().getProcessos().size();
-
-        /*
-        // MAP DE PROCESSOS QUE SERÃO FECHADOS
-        Map<Integer, String> blackList = new HashMap<Integer, String>();
-        blackList.put(0, "notepad");
-        blackList.put(1, "gedit");
-
-        //Try que pega todos os processos que estão sendo executados
+        ConnectionMySql connectionMySql = new ConnectionMySql();
+        JdbcTemplate con = new JdbcTemplate(connectionMySql.getDataSource());
+        Componentes componentes = new Componentes();
+        
+        
         try {
-            for (int i = 0; i < processList; i++) {
-                String fraseProcesso = "";
-
-                System.out.println(looca.getGrupoDeProcessos().getProcessos().get(i).getNome() + "\n");
-                fraseProcesso = looca.getGrupoDeProcessos().getProcessos().get(i).getNome();
-                log.guardarLog("Processo: " + fraseProcesso + " em execução.");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-         */
-        try {
+            Integer numero = 1;
             String espaco = "==========";
+            String tipoMaquina = "Computador-Recepção";
+            String apelidoMaquina = String.format("Máquina-%d", numero);
+            
+            
             Double ram = looca.getMemoria().getEmUso() / 1073741824.0;
             Double disco = looca.getGrupoDeDiscos().getTamanhoTotal() / 1073741824.0;
-            Integer contador = 0;
+            Boolean contador = true;
             double tamanho = new File("C:\\").getTotalSpace() - new File("C:\\").getFreeSpace();
 
             System.out.println(String.format("%s CAPTURANDO DADOS DA MÁQUINA %s", espaco, espaco));
             System.out.println(String.format("%s DADOS CAPTURADOS DA MÁQUINA: %s", espaco, espaco));
             System.out.println(String.format("%s PROCESSADOR %s MEMÓRIA RAM %s DISCO %s",
                          espaco, espaco, espaco, espaco));
-            while (contador < 50) {
+            while (contador) {
 
                 System.out.println(String.format("%s %.2f%% %s %.2f GB USADOS %s %.2f USADOS",
                         espaco, looca.getProcessador().getUso(), 
                         espaco, ram, espaco, tamanho / 1073741824.0));
+                
+                con.update("INSERT INTO maquinas VALUES (null, ?, ?, ?, ?, ?, 64)",
+                        null, tipoMaquina, 1, apelidoMaquina, 
+                        looca.getSistema().getSistemaOperacional(), 
+                        LocalDateTime.now(), looca.getSistema().getArquitetura());
+                
+                con.update("INSERT INTO componentes VALUES (null, ?, ?, ?, ?)",
+                        null, "Kingston", "RAM", "Memória Ram 8Gb 2666Mhz", 1);
+                
+                con.update("INSERT INTO registros VALUES (?, ?, ?, ?)",
+                        null, LocalDateTime.now(), ram, 1);
+                
+                numero++;
             }
 
             /* System.out.println(String.format("%.2f GB usados", ram));
@@ -56,35 +60,6 @@ public class Cli {
             e.printStackTrace();
 
         }
-
-        // while que verifica o uso de CPU e RAM(falta o disco)
-        //While da blacklist
-        /*
-        Boolean verdadeiro = true;
-        while (verdadeiro) {
-            Integer processlist = looca.getGrupoDeProcessos().getProcessos().size();
-            for (int i = 0; i < processlist; i++) {
-                try {
-                    if (blackList.containsValue(looca.getGrupoDeProcessos().getProcessos().get(i).getNome())) {
-                        if (looca.getSistema().getSistemaOperacional().equals("Windows")) {
-                            String processoFinalizado = looca.getGrupoDeProcessos().getProcessos().get(i).getNome();
-                            String killWindows = "taskkill /F /T /PID " + looca.getGrupoDeProcessos().getProcessos().get(i).getPid();
-                            Runtime.getRuntime().exec(killWindows);
-                            log.guardarLog("Processo: " + processoFinalizado + " finalizado!");
-                            looca.getGrupoDeProcessos().getProcessos().remove(i);
-                        }
-                        if (looca.getSistema().getSistemaOperacional().equals("Ubuntu")) {
-                            String killUbuntu = "kill -SIGKILL " + looca.getGrupoDeProcessos().getProcessos().get(i).getPid();
-                            Runtime.getRuntime().exec(killUbuntu);
-                            looca.getGrupoDeProcessos().getProcessos().remove(i);
-                        }
-                    }
-                } catch (Exception x) {
-                    x.printStackTrace();
-                }
-            }
-        }
-         */
     }
 
     @Override
---
+++ Cli/src/main/java/com/mycompany/loginhealthinspec/ConnectionMySql.java
@@ -0,0 +1,20 @@
+package com.mycompany.loginhealthinspec;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class ConnectionMySql {
+     private BasicDataSource dataSource;
+
+    public ConnectionMySql() {
+        dataSource = new BasicDataSource();
+
+        dataSource​.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource​.setUrl("jdbc:mysql://localhost:3306/health");
+        dataSource​.setUsername("root");
+        dataSource​.setPassword("urubu100");
+    }
+
+    public BasicDataSource getDataSource() {
+        return dataSource;
+    }
+}
