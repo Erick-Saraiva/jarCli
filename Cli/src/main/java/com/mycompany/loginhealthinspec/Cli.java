4f00203f989a34c98c8598ca71119c1c6dfe5eab
Erick Saraiva <erick.ssilva@bandtec.com.br>
2022-05-23 13:26:02 -0300

    inserts/conexao criadas (falta verificacao)

--- Cli/src/main/java/com/mycompany/loginhealthinspec/Cli.java
+++ Cli/src/main/java/com/mycompany/loginhealthinspec/Cli.java
@@ -2,63 +2,36 @@ package com.mycompany.loginhealthinspec;
 
 import com.github.britooo.looca.api.core.Looca;
 import java.io.File;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Cli {
 
     public static void main(String[] args) {
         Looca looca = new Looca();
-        ConnectionMySql connectionMySql = new ConnectionMySql();
-        JdbcTemplate con = new JdbcTemplate(connectionMySql.getDataSource());
-        Componentes componentes = new Componentes();
-        
-        
+        Inserts inserts = new Inserts();
+
         try {
-            Integer numero = 1;
             String espaco = "==========";
-            String tipoMaquina = "Computador-Recepção";
-            String apelidoMaquina = String.format("Máquina-%d", numero);
-            
-            
+
             Double ram = looca.getMemoria().getEmUso() / 1073741824.0;
-            Double disco = looca.getGrupoDeDiscos().getTamanhoTotal() / 1073741824.0;
             Boolean contador = true;
             double tamanho = new File("C:\\").getTotalSpace() - new File("C:\\").getFreeSpace();
 
             System.out.println(String.format("%s CAPTURANDO DADOS DA MÁQUINA %s", espaco, espaco));
             System.out.println(String.format("%s DADOS CAPTURADOS DA MÁQUINA: %s", espaco, espaco));
             System.out.println(String.format("%s PROCESSADOR %s MEMÓRIA RAM %s DISCO %s",
-                         espaco, espaco, espaco, espaco));
+                    espaco, espaco, espaco, espaco));
             while (contador) {
 
                 System.out.println(String.format("%s %.2f%% %s %.2f GB USADOS %s %.2f USADOS",
-                        espaco, looca.getProcessador().getUso(), 
+                        espaco, looca.getProcessador().getUso(),
                         espaco, ram, espaco, tamanho / 1073741824.0));
-                
-                con.update("INSERT INTO maquinas VALUES (null, ?, ?, ?, ?, ?, 64)",
-                        null, tipoMaquina, 1, apelidoMaquina, 
-                        looca.getSistema().getSistemaOperacional(), 
-                        LocalDateTime.now(), looca.getSistema().getArquitetura());
-                
-                con.update("INSERT INTO componentes VALUES (null, ?, ?, ?, ?)",
-                        null, "Kingston", "RAM", "Memória Ram 8Gb 2666Mhz", 1);
-                
-                con.update("INSERT INTO registros VALUES (?, ?, ?, ?)",
-                        null, LocalDateTime.now(), ram, 1);
-                
-                numero++;
+                inserts.insertMaquinas();
+                inserts.insertComponentes();
+
             }
 
-            /* System.out.println(String.format("%.2f GB usados", ram));
-            System.out.println(String.format("%.2f usados", tamanho / 1073741824.0));
-             */
         } catch (Exception e) {
-
             e.printStackTrace();
-
         }
     }
 
@@ -66,5 +39,5 @@ public class Cli {
     public String toString() {
         return "";
     }
-    
+
 }
---
+++ Cli/src/main/java/com/mycompany/loginhealthinspec/Inserts.java
@@ -0,0 +1,51 @@
+package com.mycompany.loginhealthinspec;
+
+import com.github.britooo.looca.api.core.Looca;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Random;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Inserts {
+
+    Random gerador = new Random();
+    Looca looca = new Looca();
+    ConnectionMySql connectionMySql = new ConnectionMySql();
+    JdbcTemplate con = new JdbcTemplate(connectionMySql.getDataSource());
+
+    public void insertMaquinas() throws UnknownHostException {
+
+        con.update("INSERT INTO maquinas VALUES (null, null, null, ?, ?, ?, 64)",
+                null, null, InetAddress.getLocalHost().getHostName(),
+                looca.getSistema().getSistemaOperacional(),
+                looca.getSistema().getTempoDeAtividade(),
+                looca.getSistema().getArquitetura(), gerador.nextInt(5) + 1);
+    }
+
+    public void insertComponentes() {
+
+        // Processador
+        con.update("INSERT INTO componenetes VALUES (null, ?, ?, ?)",
+                null, looca.getProcessador().getNome(),
+                looca.getProcessador().getNome() + " "
+                + looca.getProcessador().getNumeroCpusFisicas(),
+                gerador.nextInt(4) + 1);
+
+        // Memória Ram
+        con.update("INSERT INTO componenetes VALUES (null, ?, ?, ?)",
+                null, looca.getMemoria().toString(), looca.getMemoria().getTotal(),
+                gerador.nextInt(4) + 1);
+
+        // Disco
+        con.update("INSERT INTO componenetes VALUES (null, ?, ?, ?)",
+                null, "Disco", looca.getGrupoDeDiscos().getTamanhoTotal(),
+                gerador.nextInt(4) + 1);
+
+    }
+
+    /*
+    Double ram = looca.getMemoria().getEmUso() / 1073741824.0;
+    Double disco = looca.getGrupoDeDiscos().getTamanhoTotal() / 1073741824.0;
+    double tamanho = new File("C:\\").getTotalSpace() - new File("C:\\").getFreeSpace();
+     */
+}
