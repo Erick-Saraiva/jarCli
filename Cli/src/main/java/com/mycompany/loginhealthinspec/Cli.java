45788aa65d1264cbcf125b374e82a7b9f55812f7
Erick Saraiva <erick.ssilva@bandtec.com.br>
2022-05-24 00:20:15 -0300

    att

--- Cli/src/main/java/com/mycompany/loginhealthinspec/Cli.java
+++ Cli/src/main/java/com/mycompany/loginhealthinspec/Cli.java
@@ -11,16 +11,16 @@ public class Cli {
 
         try {
             String espaco = "==========";
-
+            Integer zero = 0;
             Double ram = looca.getMemoria().getEmUso() / 1073741824.0;
-            Boolean contador = true;
+            Integer contador = 5000;
             double tamanho = new File("C:\\").getTotalSpace() - new File("C:\\").getFreeSpace();
 
             System.out.println(String.format("%s CAPTURANDO DADOS DA MÁQUINA %s", espaco, espaco));
             System.out.println(String.format("%s DADOS CAPTURADOS DA MÁQUINA: %s", espaco, espaco));
             System.out.println(String.format("%s PROCESSADOR %s MEMÓRIA RAM %s DISCO %s",
                     espaco, espaco, espaco, espaco));
-            while (contador) {
+            while (zero < contador) {
 
                 System.out.println(String.format("%s %.2f%% %s %.2f GB USADOS %s %.2f USADOS",
                         espaco, looca.getProcessador().getUso(),
