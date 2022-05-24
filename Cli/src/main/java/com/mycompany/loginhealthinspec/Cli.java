d03a21d7015463194874b2efc4bc0b3d4a3fa531
Erick Saraiva <erick.ssilva@bandtec.com.br>
2022-05-24 08:49:07 -0300

    att

--- Cli/src/main/java/com/mycompany/loginhealthinspec/Cli.java
+++ Cli/src/main/java/com/mycompany/loginhealthinspec/Cli.java
@@ -2,25 +2,32 @@ package com.mycompany.loginhealthinspec;
 
 import com.github.britooo.looca.api.core.Looca;
 import java.io.File;
+import java.net.InetAddress;
+import java.time.LocalDateTime;
+import java.util.Random;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Cli {
 
     public static void main(String[] args) {
         Looca looca = new Looca();
         Inserts inserts = new Inserts();
+        Random gerador = new Random();
+        ConnectionMySql connectionMySql = new ConnectionMySql();
+        JdbcTemplate con = new JdbcTemplate(connectionMySql.getDataSource());
 
         try {
             String espaco = "==========";
-            Integer zero = 0;
             Double ram = looca.getMemoria().getEmUso() / 1073741824.0;
-            Integer contador = 5000;
-            double tamanho = new File("C:\\").getTotalSpace() - new File("C:\\").getFreeSpace();
+            Boolean contador = true;
+            double tamanho = new File("C:\\").getTotalSpace() - new File("C:\\").getFreeSpace();          
 
             System.out.println(String.format("%s CAPTURANDO DADOS DA MÁQUINA %s", espaco, espaco));
             System.out.println(String.format("%s DADOS CAPTURADOS DA MÁQUINA: %s", espaco, espaco));
             System.out.println(String.format("%s PROCESSADOR %s MEMÓRIA RAM %s DISCO %s",
                     espaco, espaco, espaco, espaco));
-            while (zero < contador) {
+
+            while (contador) {
 
                 System.out.println(String.format("%s %.2f%% %s %.2f GB USADOS %s %.2f USADOS",
                         espaco, looca.getProcessador().getUso(),
@@ -28,7 +35,7 @@ public class Cli {
                 inserts.insertMaquinas();
                 inserts.insertComponentes();
                 inserts.insertRegistros();
-
+                
             }
 
         } catch (Exception e) {
--- Cli/src/main/java/com/mycompany/loginhealthinspec/Inserts.java
+++ Cli/src/main/java/com/mycompany/loginhealthinspec/Inserts.java
@@ -4,68 +4,75 @@ import com.github.britooo.looca.api.core.Looca;
 import java.io.File;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.text.DateFormat;
 import java.time.LocalDateTime;
 import java.util.Random;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Inserts {
-    
-    Random gerador = new Random();
+
     Looca looca = new Looca();
+    Long tempoDeUso = looca.getSistema().getTempoDeAtividade();
+    Double horasLigado1 = tempoDeUso / 60.0;
+    Double horasLigado2 = horasLigado1 / 60.0;
+    String formatado = String.format("%.1f", horasLigado2);
+    String so = looca.getSistema().getSistemaOperacional();
+
+    Integer bits = looca.getSistema().getArquitetura();
+
+    String tipoMaquina = "Computador";
+    Random gerador = new Random();
     ConnectionMySql connectionMySql = new ConnectionMySql();
     JdbcTemplate con = new JdbcTemplate(connectionMySql.getDataSource());
 
     Double ram = looca.getMemoria().getEmUso() / 1073741824.0;
     Double disco = looca.getGrupoDeDiscos().getTamanhoTotal() / 1073741824.0;
     double tamanho = new File("C:\\").getTotalSpace() - new File("C:\\").getFreeSpace();
-    
-    public void insertMaquinas() throws UnknownHostException {
+    double tamanhoTotal = new File("C:\\").getTotalSpace();
 
-        con.update("INSERT INTO maquinas VALUES (null, null, null, ?, ?, ?, 64)",
-                null, null, InetAddress.getLocalHost().getHostName(),
-                looca.getSistema().getSistemaOperacional(),
-                looca.getSistema().getTempoDeAtividade(),
-                looca.getSistema().getArquitetura(), gerador.nextInt(5) + 1);
+    public void insertMaquinas() throws UnknownHostException {
+        String hostName = InetAddress.getLocalHost().getHostName();
+        con.update("INSERT INTO maquinas VALUES (?, ?, ?, ?, ?, ?)",
+                null, tipoMaquina, hostName, so, bits, 1);
     }
 
     public void insertComponentes() {
 
         // Processador
-        con.update("INSERT INTO componenetes VALUES (null, ?, ?, ?)",
+        con.update("INSERT INTO componentes VALUES (?, ?, ?, ?);",
                 null, looca.getProcessador().getNome(),
-                looca.getProcessador().getNome() + " "
-                + looca.getProcessador().getNumeroCpusFisicas(),
+                looca.getProcessador().getFabricante() + " "
+                + looca.getProcessador().getMicroarquitetura(),
                 gerador.nextInt(4) + 1);
 
         // Memória Ram
-        con.update("INSERT INTO componenetes VALUES (null, ?, ?, ?)",
-                null, looca.getMemoria().toString(), looca.getMemoria().getTotal(),
+        con.update("INSERT INTO componentes VALUES (?, ?, ?, ?);",
+                null, "RAM", looca.getMemoria().toString(),
                 gerador.nextInt(4) + 1);
 
         // Disco
-        con.update("INSERT INTO componenetes VALUES (null, ?, ?, ?)",
-                null, "Disco", looca.getGrupoDeDiscos().getTamanhoTotal(),
+        con.update("INSERT INTO componentes VALUES (?, ?, ?, ?);",
+                null, "Disco", tamanhoTotal,
                 gerador.nextInt(4) + 1);
 
-        
     }
-    
+
     public void insertRegistros() {
-        
+
         // Processador
-        con.update("INSERT INTO registros VALUES (null, ?, ?, ?)",
-                null, LocalDateTime.now(), looca.getProcessador().getUso(), 
+        con.update("INSERT INTO registros VALUES (?, ?, ?, ?)",
+                null, LocalDateTime.now(), looca.getProcessador().getUso(),
                 gerador.nextInt(2) + 1);
-        
+
         // Memória
-        con.update("INSERT INTO registros VALUES (null, ?, ?, ?)",
+        con.update("INSERT INTO registros VALUES (?, ?, ?, ?)",
                 null, LocalDateTime.now(), ram,
-                gerador.nextInt(2)+1);
-        
+                gerador.nextInt(2) + 1);
+
         // Disco
-        con.update("INSERT INTO registros VALUES (null, ?, ?, ?)",
-                null, LocalDateTime.now(), tamanho, gerador.nextInt(2)+1);
-        
+        con.update("INSERT INTO registros VALUES (?, ?, ?, ?)",
+                null, LocalDateTime.now(), tamanho, gerador.nextInt(2) + 1);
+
     }
 
 }
