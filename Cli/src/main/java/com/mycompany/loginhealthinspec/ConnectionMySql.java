5fff55352df4aeabd545fe6fc951a698741431b3
Erick Saraiva <erick.ssilva@bandtec.com.br>
2022-05-24 08:57:01 -0300

    att

--- Cli/src/main/java/com/mycompany/loginhealthinspec/ConnectionMySql.java
+++ Cli/src/main/java/com/mycompany/loginhealthinspec/ConnectionMySql.java
@@ -10,7 +10,7 @@ public class ConnectionMySql {
         
 
         dataSource​.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource​.setUrl("jdbc:mysql://localhost:3306/health?useTimezone=true&serverTimezone=UTC");
+        dataSource​.setUrl("jdbc:mysql://0.0.0.0:3306/health?useTimezone=true&serverTimezone=UTC");
         dataSource​.setUsername("root");
         dataSource​.setPassword("urubu100");
     }
