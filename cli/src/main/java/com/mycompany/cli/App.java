60a19c53fa33360fe99c9626082b01e1b30c4862
Erick Saraiva <erick.ssilva@bandtec.com.br>
2022-05-28 22:21:45 -0300

    att

--- cli/src/main/java/com/mycompany/cli/App.java
+++ cli/src/main/java/com/mycompany/cli/App.java
@@ -5,12 +5,16 @@
 package com.mycompany.cli;
 
 import com.github.britooo.looca.api.core.Looca;
+import com.github.britooo.looca.api.group.discos.Volume;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
@@ -20,27 +24,85 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class App {
 
     public static void main(String[] args) throws UnknownHostException {
-        String hostName = InetAddress.getLocalHost().getHostName();
         ConnectionAzure azure = new ConnectionAzure();
-        JdbcTemplate con = new JdbcTemplate(azure.getDataSource());
-        Scanner scan = new Scanner(System.in);
-        Looca looca = new Looca();
-        Inserts inserts = new Inserts();
-        ResultSet resultSetEmail = null;
-        String so = looca.getSistema().getSistemaOperacional();
-        Integer bits = looca.getSistema().getArquitetura();
 
         try {
             Boolean contador = true;
+            Looca looca = new Looca();
+            Double ram = looca.getMemoria().getEmUso() / 1073741824.0;
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
             java.sql.Connection connection = DriverManager.getConnection(azure.connectionUrl);
-            Statement statement = connection.createStatement();
-            System.out.println("Conexão obtida com sucesso!");          
+            JdbcTemplate con = new JdbcTemplate(azure.getDataSource());
+            System.out.println("Conexão obtida com sucesso!");
 
-                while (contador) {
-                        inserts.msg();
+
+
+            while (contador) {
+
+                String espaco = "==========";
+                List<Volume> volumes = looca.getGrupoDeDiscos().getVolumes();
+                Double discoDisponivel = 0.0;
+
+                for (Volume volume : volumes) {
+                    discoDisponivel += volume.getDisponivel();
                 }
-            
+
+                List<Maquinas> listaMaquinas = con.query("SELECT idMaquina FROM maquinas;",
+                        new BeanPropertyRowMapper<>(Maquinas.class));
+
+                Maquinas maquina = listaMaquinas.get(0);
+
+                List<ComponentesMaquinas> idCpu = con.query("SELECT idComponenteMaquina from componentes_has_maquinas WHERE fkMaquina = " + maquina.getIdMaquina() + " AND fkComponente = '3';",
+                        new BeanPropertyRowMapper<>(ComponentesMaquinas.class));
+
+                List<ComponentesMaquinas> idMemoria = con.query("SELECT idComponenteMaquina from componentes_has_maquinas WHERE fkMaquina = " + maquina.getIdMaquina() + " AND fkComponente = '1';",
+                        new BeanPropertyRowMapper<>(ComponentesMaquinas.class));
+
+                List<ComponentesMaquinas> idDisco = con.query("SELECT idComponenteMaquina from componentes_has_maquinas WHERE fkMaquina = " + maquina.getIdMaquina() + " AND fkComponente = '2';",
+                        new BeanPropertyRowMapper<>(ComponentesMaquinas.class));
+
+                BigDecimal consumoRAM = new BigDecimal(looca.getMemoria().getEmUso().doubleValue() / 1073741824).setScale(2, RoundingMode.HALF_EVEN);
+                BigDecimal percentualCPU = new BigDecimal(looca.getProcessador().getUso()).setScale(2, RoundingMode.HALF_EVEN);
+                BigDecimal consumoDisco = new BigDecimal(discoDisponivel / 1e+9).setScale(0, RoundingMode.HALF_EVEN);
+                Date data = new Date();
+                SimpleDateFormat formatar = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String dataFormatada = formatar.format(data);
+
+                System.out.println(String.format("%s %.2f%% %s %.2f GB USADOS %s %.2f USADOS",
+                        espaco, looca.getProcessador().getUso(),
+                        espaco, ram, espaco, consumoDisco));
+
+                // Registros:
+                String insertReg = "INSERT INTO registros (fkComponenteMaquina, fkComponente, fkMaquina, dataHora, totalUsado) VALUES (?, ?, ?, ?, ?)";
+
+                //CPU
+                con.update(insertReg,
+                        idCpu.get(0).getIdComponenteMaquina(),
+                        3,
+                        maquina.getIdMaquina(),
+                        dataFormatada,
+                        percentualCPU
+                );
+
+                //RAM
+                con.update(insertReg,
+                        idMemoria.get(0).getIdComponenteMaquina(),
+                        1,
+                        maquina.getIdMaquina(),
+                        dataFormatada,
+                        consumoRAM
+                );
+
+                //DISCO
+                con.update(insertReg,
+                        idDisco.get(0).getIdComponenteMaquina(),
+                        2,
+                        maquina.getIdMaquina(),
+                        dataFormatada,
+                        consumoDisco
+                );
+
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
--- cli/src/main/java/com/mycompany/cli/Inserts.java
+++ cli/src/main/java/com/mycompany/cli/Inserts.java
@@ -1,121 +1,99 @@
 package com.mycompany.cli;
 
 import com.github.britooo.looca.api.core.Looca;
+import java.awt.Color;
 import java.io.File;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.sql.ResultSet;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Random;
+import java.sql.DriverManager;
+import java.util.List;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Inserts {
-
-    ConnectionAzure azure = new ConnectionAzure();
-    Looca looca = new Looca();
-    Long tempoDeUso = looca.getSistema().getTempoDeAtividade();
-    Double horasLigado1 = tempoDeUso / 60.0;
-    Double horasLigado2 = horasLigado1 / 60.0;
-    String formatado = String.format("%.1f", horasLigado2);
-    String so = looca.getSistema().getSistemaOperacional();
-    Integer bits = looca.getSistema().getArquitetura();
-    ResultSet resultSetEmail = null;
-
-    String tipoMaquina = "Computador";
-    Random gerador = new Random();
-
-    Double ram = looca.getMemoria().getEmUso() / 1073741824.0;
-    Double disco = looca.getGrupoDeDiscos().getTamanhoTotal() / 1073741824.0;
-    double tamanho = new File("C:\\").getTotalSpace() - new File("C:\\").getFreeSpace();
-    double tamanhoTotal = new File("C:\\").getTotalSpace();
-
-    public void msg() throws UnknownHostException {
-        Boolean contador = true;
-        String espaco = "==========";
-        String so = looca.getSistema().getSistemaOperacional();
-        Integer bits = looca.getSistema().getArquitetura();
-        ConnectionAzure azure = new ConnectionAzure();
-        JdbcTemplate con = new JdbcTemplate(azure.getDataSource());
-        String hostName = InetAddress.getLocalHost().getHostName();
-        System.out.println(String.format("%s CAPTURANDO DADOS DA MÁQUINA %s", espaco, espaco));
-        System.out.println(String.format("%s DADOS CAPTURADOS DA MÁQUINA: %s", espaco, espaco));
-        System.out.println(String.format("%s PROCESSADOR %s MEMÓRIA RAM %s DISCO %s",
-                espaco, espaco, espaco, espaco));
         
-        String insertComp = "INSERT INTO componentes (nomeComponente) VALUES (?)";
-        
+    public void insertMaquinas() {
+    
+    
+    try {
+            ConnectionAzure azure = new ConnectionAzure();
+            Looca looca = new Looca();
+            Double ram = looca.getMemoria().getEmUso() / 1073741824.0;
+            Double disco = looca.getGrupoDeDiscos().getTamanhoTotal() / 1073741824.0;
+            String so = looca.getSistema().getSistemaOperacional();
+            Integer bits = looca.getSistema().getArquitetura();
+            String hostName = InetAddress.getLocalHost().getHostName();
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+            java.sql.Connection connection = DriverManager.getConnection(azure.connectionUrl);
+            JdbcTemplate con = new JdbcTemplate(azure.getDataSource());
+            System.out.println("Conexão obtida com sucesso!");
 
-        while (contador) {
+            List<Funcionario> listaLoginFuncionario = con.query(
+                    "SELECT idFuncionario, email, fkHospital, senha FROM funcionario;",
+                    new BeanPropertyRowMapper<>(Funcionario.class));
 
-                                System.out.println(String.format("%s %.2f%% %s %.2f GB USADOS %s %.2f USADOS",
-                            espaco, looca.getProcessador().getUso(),
-                            espaco, ram, espaco, tamanho / 1073741824.0));
+            if (listaLoginFuncionario.isEmpty()) {
+                System.out.println("Erro");
+            } else {
 
-                    // Máquinas
-                    String insertMaq = "INSERT INTO maquinas (tipoMaquina, fkTecnico, nomeMaquina, sistemaOperacional, arquitetura) VALUES (?, ?, ?, ?, ?)";
+                Funcionario funcionario = listaLoginFuncionario.get(0);
 
-                    con.update(insertMaq,
+                List<Maquinas> listaMaquinas = con.query("SELECT * FROM maquinas WHERE fkHospital = " + funcionario.getFkHospital() + ";",
+                        new BeanPropertyRowMapper<>(Maquinas.class));
+
+                //System.out.println(listaMaquinas.get(0).toString());
+                if (listaMaquinas.isEmpty()) {
+
+                    String insertMaquinas = "INSERT INTO maquinas (tipoMaquina, nomeMaquina, sistemaOperacional, arquitetura, fkHospital) VALUES (?, ?, ?, ?, ?);";
+                    con.update(insertMaquinas,
                             "Computador",
-                            gerador.nextInt(2) + 1,
                             hostName,
                             so,
-                            bits
-                    );  
+                            bits,
+                            funcionario.getFkHospital()
+                    );
+                    listaMaquinas = con.query("SELECT * FROM maquinas WHERE fkHospital = " + funcionario.getFkHospital() + ";",
+                        new BeanPropertyRowMapper<>(Maquinas.class));
                     
+                    Maquinas maquinas = listaMaquinas.get(0);
                     
-
-                    // Componentes:
-                    String insertCompMaq = "INSERT INTO componentes_has_maquinas (totalComponente, unidadeMedida, fkComponente, fkMaquina) VALUES (?, ?, ?, ?);";
+                    String insertCompMaq = "INSERT INTO componentes_has_maquinas (fkComponente, fkMaquina, totalComponente, unidadeMedida) VALUES (?, ?, ?, ?);";
 
                     //CPU
                     con.update(insertCompMaq,
+                            3,
+                            maquinas.getIdMaquina(),
                             looca.getProcessador().getFrequencia(),
-                            "Ghz",
-                            13,
-                            gerador.nextInt(2) + 1
+                            "Ghz"
                     );
+
                     //RAM
                     con.update(insertCompMaq,
-                            String.format("Memória de %.1f", 
+                            1,
+                            maquinas.getIdMaquina(),
+                            String.format("Memória de %.1f Gb",
                                     ram),
-                            "Gb",
-                            12,
-                            gerador.nextInt(2) + 1
+                            "Gb"
                     );
+
                     //DISCO
                     con.update(insertCompMaq,
+                            2,
+                            maquinas.getIdMaquina(),
                             String.format("Disco de %.1f",
-                                    tamanhoTotal),
-                            "Gb",
-                            14,
-                            gerador.nextInt(2) + 1
+                                    disco),
+                            "Gb"
                     );
 
-                    // Registros:
-                    String insertReg = "INSERT INTO registros (dataHora, totalUsado, fkComponenteMaquina) VALUES (?, ?, ?)";
 
-                    //CPU
-                    con.update(insertReg,
-                            LocalDateTime.now(),
-                            looca.getProcessador().getUso(),
-                            gerador.nextInt(2) + 1
-                    );
+                } else {
+                    System.out.println("Máquina já inserida!");
+                }
+            }
 
-                    //RAM
-                    con.update(insertReg,
-                            LocalDateTime.now(),
-                            looca.getMemoria().getEmUso(),
-                            gerador.nextInt(2) + 1
-                    );
-
-                    //DISCO
-                    con.update(insertReg,
-                            LocalDateTime.now(),
-                            tamanho,
-                            gerador.nextInt(2) + 1);
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
+    
     }
+    
 }
